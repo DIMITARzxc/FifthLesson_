5a80ee1162d69c0082e204f79a3444eff3ad0aff
dimitar.nedilkovski@gmail.com <dimitar.nedilkovski@gmail.com>
2019-08-22 19:38:15 +0300

    Commit when i add Comment with palendrom code

--- FifthLesson_/Program.cs
+++ FifthLesson_/Program.cs
@@ -6,6 +6,24 @@ using System.Threading.Tasks;
 
 namespace FifthLesson_
 {
+    /*public static bool  Compare (string s)
+         {
+
+             for (int i = 0; i < s.Length / 2; i++)
+
+                 if (s[i] != s[s.Length - i - 1])
+                     return false;
+             return true;
+         }
+         static void Main()
+         {
+             Console.WriteLine("Write some word");
+             string s = Console.ReadLine();
+             if (Compare(s))
+                 Console.WriteLine("This word is palindrom");
+             else Console.WriteLine("This word is not palindrom");
+             Console.ReadLine();
+         }*/
     class Program
     {
         static int[] arr = { 123, 32, 12, 22, 41 };
